09fcf99d7d60e582015794d17cdfe614f698c285
Matis HERRMANN <matis_herrmann@hotmail.fr>
2024-10-07 16:40:54 +0200

    adding files

--- applet/src/main/java/fr/polytech/unice/App.java
+++ applet/src/main/java/fr/polytech/unice/App.java
@@ -2,13 +2,10 @@ package fr.polytech.unice;
 
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         CardApplet.install(null, (short) 0, (byte) 0);
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
--- vending-machine/src/main/java/fr/polytech/unice/App.java
+++ vending-machine/src/main/java/fr/polytech/unice/App.java
@@ -1,13 +1,38 @@
 package fr.polytech.unice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    static final int MAX = 6;
+
+    public static void main(String[] args) {
+        List<VendorMachine> machines = new ArrayList<>();
+
+        System.out.println("Initializing vending machines...");
+        for (int i = 0; i < MAX; i++) {
+            VendorMachine machine = new VendorMachine(i);
+            machines.add(machine);
+        }
+
+        while (true) {
+            System.out.println("Choose a vending machine to stop (0 to " + (MAX - 1) + "): ");
+            int choice = Integer.parseInt(System.console().readLine());
+
+            VendorMachine machine = machines.get(choice);
+            machine.selected();
+
+            System.out.println("Choose a product to buy ");
+            String product = System.console().readLine();
+
+            if (machine.buy(product)) {
+                System.out.println("Product bought!");
+            } else {
+                System.out.println("Product not available!");
+            }
+        }
     }
 }
