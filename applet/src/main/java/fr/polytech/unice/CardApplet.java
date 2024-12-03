e26eb56edfb195fb1c06edd0160971b05777b97c
amandinemart1 <amandine-martin@sfr.fr>
2024-12-03 10:32:34 +0100

    addition publib private key

--- applet/src/main/java/fr/polytech/unice/CardApplet.java
+++ applet/src/main/java/fr/polytech/unice/CardApplet.java
@@ -1,6 +1,18 @@
 package fr.polytech.unice;
 
-import javacard.framework.*;
+import java.security.interfaces.RSAPublicKey;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.OwnerPIN;
+import javacard.framework.Util;
+import javacard.security.KeyPair;
+import javacard.security.RSAPrivateCrtKey;
+import javacard.security.RSAPublicKey;
+import javacardx.crypto.Cipher;
+
 
 public class CardApplet extends Applet {
 
@@ -23,8 +35,8 @@ public class CardApplet extends Applet {
     // maximum number of incorrect tries before the PIN is blocked | 0x03 = 3
     static final byte PIN_TRY_LIMIT = (byte) 0x03;
 
-    // maximum size PIN | 0x06 = 6
-    static final byte MAX_PIN_SIZE = (byte) 0x06;
+    // maximum size PIN | 0x04 = 4
+    static final byte MAX_PIN_SIZE = (byte) 0x04;
 
     // signal that the PIN verification failed
     static final short SW_VERIFICATION_FAILED = 0x6312;
@@ -41,8 +53,14 @@ public class CardApplet extends Applet {
     // signal the balance becomes negative
     static final short SW_NEGATIVE_BALANCE = 0x6A85;
 
+    // RSA key size
+    static final short KEY_BITS = 512;
+
     // instance of the PIN
     OwnerPIN pin;
+
+    // RSA key pair
+    private KeyPair keyPair;
 
     // balance of the wallet
     short balance;
@@ -74,6 +92,11 @@ public class CardApplet extends Applet {
      */
     public static void install(byte[] bArray, short bOffset, byte bLength) throws ISOException {
         new CardApplet(bArray, bOffset, bLength).register();
+    }
+
+    private void generateKeyPair() {
+        this.keyPair = new KeyPair(KeyPair.ALG_RSA, KEY_BITS);
+        this.keyPair.genKeyPair();
     }
 
     /**
@@ -214,5 +237,49 @@ public class CardApplet extends Applet {
 
         // debit the wallet
         this.balance -= amount;
+    }
+
+    private static final byte[] ASN1_SHA256 = {
+        (byte) 0x30, (byte) 0x31, (byte) 0x30, (byte) 0x0d, (byte) 0x06, (byte) 0x09, (byte) 0x60,
+        (byte) 0x86, (byte) 0x48, (byte) 0x01, (byte) 0x65, (byte) 0x03, (byte) 0x04, (byte) 0x02,
+        (byte) 0x01, (byte) 0x05, (byte) 0x00, (byte) 0x04, (byte) 0x20
+    };
+
+    // sign the datasent by the client using the private key 
+    private void sign (APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        Cipher cipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
+        cipher.init(keyPair.getPrivate(), Cipher.MODE_ENCRYPT);
+        short inputLength = buffer[ISO7816.OFFSET_LC];
+        byte[] markedData = new byte[(short)(inputLength + ASN1_SHA256.length)];
+        Util.arrayCopy(ASN1_SHA256, (short) 0, markedData, (short) 0, (short) ASN1_SHA256.length);
+        Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, markedData, (short) ASN1_SHA256.length, inputLength);
+        byte dataLength = buffer[ISO7816.OFFSET_LC];
+        short outLength = cipher.doFinal(markedData, (short) 0, (short) markedData.length, buffer, ISO7816.OFFSET_CDATA);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, outLength)
+    }
+
+    // send back the public key to the client 
+    private void getPublicKey(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        short offset = ISO7816.OFFSET_CDATA;
+        RSAPublicKey key = (RSAPublicKey) keyPair.getPublic();
+        short expLen = key.getExponent(buffer, (short) (offset + 2));
+        Util.setShort(buffer, offset, expLen);
+        short modLen = key.getModulus(buffer, (short) (offset + 4 + expLen));
+        Util.setShort(buffer, (short) (offset + 2 + expLen), modLen);
+        apdu.setOutgoingAndSend(offset, (short) (4 + expLen + modLen));
+    }
+
+    // get private key from the client 
+    private void getPrivateKey(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        short offset = ISO7816.OFFSET_CDATA;
+        RSAPrivateCrtKey key = (RSAPrivateCrtKey) keyPair.getPrivate();
+        short pLen = key.getP(buffer, (short) (offset + 2));
+        Util.setShort(buffer, offset, pLen);
+        short qLen = key.getQ(buffer, (short) (offset + 4 + pLen));
+        Util.setShort(buffer, (short) (offset + 2 + pLen), qLen);
+        apdu.setOutgoingAndSend(offset, (short) (4 + pLen + qLen));
     }
 }
