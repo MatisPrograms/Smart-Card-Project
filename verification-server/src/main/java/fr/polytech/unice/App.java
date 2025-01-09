512ccf48dc3c89ff26ec65c723e65f5c410659fe
Matis HERRMANN <matis_herrmann@hotmail.fr>
2025-01-09 13:27:48 +0100

    When running server, now asks for pin in System.in or via arguments

--- verification-server/src/main/java/fr/polytech/unice/App.java
+++ verification-server/src/main/java/fr/polytech/unice/App.java
@@ -1,6 +1,9 @@
 package fr.polytech.unice;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
+import static polytech.CardApplet.*;
 
 /**
  * Run the verification server.
@@ -8,7 +11,12 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         int pinIndex = Arrays.asList(args).indexOf("--pin") + 1;
-        String pin = pinIndex >= args.length ? "1234" : args[pinIndex];
+        String pin = pinIndex >= args.length ? askForPIN() : args[pinIndex];
         new VerificationServer(pin);
+    }
+
+    private static String askForPIN() {
+        System.out.print("Enter a " + PIN_LENGTH + "-digit PIN for the card: ");
+        return new Scanner(System.in).next("\\d{" + PIN_LENGTH + "}");
     }
 }
